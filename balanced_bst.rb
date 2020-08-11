b4b4c4c387ce7cd2d1238543b07e7fceacdde9c0
programmurr <conorjm89@gmail.com>
2020-08-11 20:18:37 +0100

    Find, insert and delete methods working, as is inorder traversal

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -33,6 +33,63 @@ class Tree
     root_node
   end
 
+  def insert(root_node, insert_node)
+    return root_node = insert_node if root_node.nil?
+
+    if root_node.data < insert_node.data
+      if root_node.right
+        insert(root_node.right, insert_node)
+      else
+        root_node.right = insert_node
+      end
+    else
+      if root_node
+        insert(root_node.left, insert_node)
+      else
+        root_node.left = insert_node
+      end
+    end
+  end
+
+  def minimum_value_node(node)
+    node = node.left until node.left.nil?
+    node
+  end
+
+  def delete(node, key)
+    return node unless node
+
+    if key < node.data
+      node.left = delete(node.left, key)
+    elsif key > node.data
+      node.right = delete(node.right, key)
+    else
+      if node.left.nil?
+        temp = node.right
+        node = nil
+        return temp
+      elsif node.right.nil?
+        temp = node.left
+        node = nil
+        return temp
+      end
+      temp = minimum_value_node(node.right)
+      node.data = temp.data
+      node.right = delete(node.right, temp.data)
+    end
+    node
+  end
+
+  def find(node, key)
+    return node if node.nil? || node.data == key
+
+    if node.data < key
+      find(node.right, key)
+    else
+      find(node.left, key)
+    end
+  end
+
   def pretty_print(node = root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│ ' : ' '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -46,9 +103,18 @@ class Tree
     pre_order(node.left)
     pre_order(node.right)
   end
+
+  def in_order(node)
+    if node
+      in_order(node.left)
+      print "#{node.data}, "
+      in_order(node.right)
+    end
+  end
 end
 
-tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
-
-p tree.pre_order(tree.root)
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree.pretty_print
+tree.delete(tree.root, 8)
+puts '-------------------'
 tree.pretty_print
